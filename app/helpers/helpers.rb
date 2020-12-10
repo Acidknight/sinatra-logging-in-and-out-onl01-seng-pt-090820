18e52d8dc29fa5610a3e0c8b41b81cef7fb486a3
Anthony Leon <acidknights89@gmail.com>
2020-12-10 08:55:00 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
   def self.current_user(session_hash)
-    @user = User.find(session_hash[:user_id])
+    User.find(session_hash["user_id"])
   end
 
   def self.is_logged_in?(session_hash)
-    !!session_hash[:user_id]
+    !!session_hash["user_id"]
   end
-end
+end 
