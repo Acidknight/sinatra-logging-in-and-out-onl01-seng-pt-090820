79cbc7ea211336af0d0a8f7931a71fdc50a52070
Anthony Leon <acidknights89@gmail.com>
2020-12-10 09:07:37 +0000

    Automatically backed up by Learn

--- db/migrate/20201210090017_users.rb
+++ db/migrate/20201210090017_users.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Migration[5.1]
   def up
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :username
       t.string :password
       t.float :balance
