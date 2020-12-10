1f6ec0d78680319bff7182c0c68cf9601f0796f9
Anthony Leon <acidknights89@gmail.com>
2020-12-10 08:54:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    user = User.find_by(username: params["username"], password: params["password"])
-    if user
-      session[:user_id] = user.id
+
+    @user = User.find_by(username: params["username"])
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
       redirect to '/account'
     else
       erb :error
@@ -21,10 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+
     erb :account
   end
 
   get '/logout' do
+
     session.clear
     redirect to '/'
   end
